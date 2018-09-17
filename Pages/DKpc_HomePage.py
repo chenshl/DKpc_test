f1c3fee7198a7498b3ce7f6d0c75c50c53c13f58
Administrator <35643856@qq.com>
2018-09-17 18:05:21 +0800

    登录滑块验证

--- Pages/DKpc_HomePage.py
+++ Pages/DKpc_HomePage.py
@@ -3,26 +3,60 @@
 # @date   : 2018/08/25 10:16
 """PC首页"""
 
+import time
 from selenium.webdriver.common.by import By
 from BaseSe.Selenium3 import Pyse
 
 class DKpc_HomePage(Pyse):
 
     """定位器"""
-    # 币币交易tab页面
+    # tab页面
+    # C2C
+    tab_c2c = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/c2c']")
+    # 币币交易
     tab_Switch_exchange = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/exchange']")
+    # 个人中心
+    tab_uc_assets = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/uc/assets']")
+    # 活动中心
+    tab_activity_lock = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/activity/lock']")
+    # SLU生态
+    tab_slust = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/slust']")
     # 登录按钮
-    # button_jump_login = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/login']//li[contains(text(),'登录')]")
     button_jump_login = (By.XPATH, "//ul[@class='ivu-menu']//a[@href='#/login']")
 
     def open(self):
         # 调用Selenium3中的_open()方法打开连接
         self._open(self.base_url, self.pagetitle)
 
-    # 未登录状态页面预览
-    def open_Switch_exchange(self):
-        self.find_element(*self.tab_Switch_exchange).click()
 
-    # 跳转登录页面
+    def open_Switch_exchange(self):
+        """
+        @description: 预览未登录页面
+        :return: 
+        """
+        self.find_element(*self.tab_c2c).click()
+        time.sleep(1)
+        self.find_element(*self.tab_Switch_exchange).click()
+        time.sleep(1)
+        self.find_element(*self.tab_activity_lock).click()
+        time.sleep(1)
+        self.find_element(*self.tab_uc_assets).click()
+        time.sleep(1)
+        self.find_element(*self.tab_slust).click()
+        time.sleep(1)
+
+
     def open_loginPage(self):
+        """
+        @description: 跳转登录页面
+        :return: 
+        """
         self.find_element(*self.button_jump_login).click()
+
+
+    def open_uc_assets_page(self):
+        """
+        @description: 跳转个人中心页面
+        :return: 
+        """
+        self.find_element(*self.tab_uc_assets).click()
--- TestCase/Test_DKpc.py
+++ TestCase/Test_DKpc.py
@@ -15,33 +15,36 @@ from time import sleep
 
 class DKpcCase(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # 选择默认浏览器
-        self.driver = Pyse.browser()
+        cls.driver = Pyse.browser()
         # 获取Excel表格中的url
-        self.url = DKpcData.UrlXlsx()
+        cls.url = DKpcData.UrlXlsx()
         # 获取Excel表格中的title
-        self.title = DKpcData.titleXlsx()
+        cls.title = DKpcData.titleXlsx()
 
-    def tearDown(self):
-        driver = self.driver
+        # 打开测试主页
+        cls.DKpc_startPage = DKpc_HomePage(cls.driver, cls.url, cls.title)
+        cls.DKpc_startPage.open()
+
+    @classmethod
+    def tearDownClass(cls):
+        driver = cls.driver
         sleep(5)
         driver.quit()
 
 
     def test_DKpc_HomePage_view(self):
-        """测试预览页面"""
-        DKpc_click = DKpc_HomePage(self.driver, self.url, self.title)
-        DKpc_click.open()
-        DKpc_click.open_Switch_exchange()
+        """测试预览页面,未登录状态"""
+        self.DKpc_startPage.open_Switch_exchange()
 
     def test_login(self):
         """用户登录"""
-        DKpc_click = DKpc_HomePage(self.driver, self.url, self.title)
-        DKpc_click.open()
-        DKpc_click.open_loginPage()
+        self.DKpc_startPage.open_loginPage()
         DKpc_login = DKpc_LoginPage(self.driver, self.url, self.title)
         DKpc_login.login_put()
+        self.DKpc_startPage.open_uc_assets_page()
 
 
 
